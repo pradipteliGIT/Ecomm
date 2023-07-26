dfb5df68b9a2153c2065758294d2e098068c67a8
Pradip Teli <vptelipradip@gmail.com>
2023-07-26 16:46:11 +0530

    End of section 3

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,24 +10,40 @@ namespace API.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class ProductsController : ControllerBase
-    { 
-        private readonly StoreContext _context;
-        public ProductsController(StoreContext context)
+    {
+        private readonly IProductRepository _repo;
+
+        public ProductsController(IProductRepository repo)
         {
-            _context = context;
+            _repo = repo;
         }
+        
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _context.Products.ToListAsync();
-            return products;
+            var products = await _repo.GetProductsAsync();
+            return Ok(products);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _repo.GetProductByIdAsync(id);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<List<ProductBrand>>> GetBrands()
+        {
+            var brands = await _repo.GetProductBrandsAsync();
+            return Ok(brands);
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<List<ProductType>>> GetTypes()
+        {
+            var types = await _repo.GetProductTypesAsync();
+            return Ok(types);
         }
     }
 }
---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IProductRepository
+    {
+        public Task<Product> GetProductByIdAsync(int id);
+        public Task<IReadOnlyList<Product>> GetProductsAsync();
+        public Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync();
+        public Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
+    }
+}
