f8e7bc53176da1287fdf530fa1f16e436b5344d7
Pradip Teli <vptelipradip@gmail.com>
2023-08-08 15:05:31 +0530

    End of section 10

---
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ShopService } from '../shop.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/shared/models/product';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.scss'],
+})
+export class ProductDetailsComponent implements OnInit {
+  product?: Product;
+  constructor(
+    private shopService: ShopService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+  ngOnInit(): void {
+    this.loadProduct();
+  }
+
+  loadProduct() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (id) {
+      this.shopService.getProduct(+id).subscribe({
+        next: (response) => (this.product = response),
+        error: (error) => console.log('Error while getting single product'),
+      });
+    }
+  }
+}
