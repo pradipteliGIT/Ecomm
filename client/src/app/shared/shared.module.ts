8e9b2a823dc2f0d4a0498f125766d5432dbb94db
Pradip Teli <vptelipradip@gmail.com>
2023-08-17 14:01:36 +0530

    End of section 14

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -4,15 +4,21 @@ import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { PaginationHeaderComponent } from './pagination-header/pagination-header.component';
 import { PagerComponent } from './pager/pager.component';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { OrderTotalsComponent } from './order-totals/order-totals.component';
 
 @NgModule({
-  declarations: [PaginationHeaderComponent, PagerComponent],
+  declarations: [
+    PaginationHeaderComponent,
+    PagerComponent,
+    OrderTotalsComponent,
+  ],
   imports: [CommonModule, PaginationModule.forRoot(), CarouselModule.forRoot()],
   exports: [
     PaginationModule,
     PaginationHeaderComponent,
     PagerComponent,
     CarouselModule,
+    OrderTotalsComponent,
   ],
 })
 export class SharedModule {}
